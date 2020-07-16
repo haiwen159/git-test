716c6375784449ee2371ce1fd32a0f4d38a78b36
haiwen <1208392843@qq.com>
2020-07-17 01:57:41 +0800

    把本地工程添加到git

---
+++ git/src/cn/itcast/git.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+public class git {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+		System.out.println("hello git");
+		System.out.println("hello git");
+		System.out.println("hello git");
+		System.out.println("hello git");
+		System.out.println("hello git");
+	}
+}
